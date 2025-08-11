deb247b9f54dd5097ad9207b72a99fc8f8f934d0
Darren Kent <darrenkent88@gmail.com>
2025-08-11 11:18:15 +0700

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import { ActorProvider, AgentProvider } from "@ic-reactor/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { canisterId, idlFactory } from "./declarations/backend";
+import "./index.css";
+import App from "./App";
+
+console.log("canisterId:", canisterId); // Debug line
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <AgentProvider withProcessEnv>
+      <ActorProvider idlFactory={idlFactory} canisterId={canisterId}>
+        <App />
+      </ActorProvider>
+    </AgentProvider>
+  </React.StrictMode>
+);
