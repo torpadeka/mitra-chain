b1759a5a2c12c72bd3aa98c4b5d99a3a2a319783
Christopher Hardy Gunawan <christopher.hygunawan@gmail.com>
2025-08-20 09:41:49 +0700

    Uninstall p5 module

--- src/components/franchise-details.tsx
+++ src/components/franchise-details.tsx
@@ -39,7 +39,7 @@ export function FranchiseDetails({ franchise }: FranchiseDetailsProps) {
         </CardHeader>
         <CardContent>
           <p className="text-gray-700 leading-relaxed">
-            {franchise.licenseDuration.years} years
+            {franchise.licenseDuration.Years} years
           </p>
         </CardContent>
       </Card>
